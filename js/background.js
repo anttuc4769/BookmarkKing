badb2e960af39404532a8f6c251c59ad5c04a05a
Anthony Tuccitto <tonytux1@hotmail.com>
2020-02-16 11:15:40 -0500

    1.0 working

--- js/background.js
+++ js/background.js
@@ -5,8 +5,16 @@ chrome.runtime.onMessage.addListener(function (rq, sender, sendResponse) {
                 sendResponse({ response: data });
             });
             break;
+        case "gb-id":
+            $.get(rq.url, function (data) {
+                sendResponse({ response: data });
+            });
+            break;
         case "kw":
-            InsertTags(rq.url, rq.json, rq.dbKey, rq.bookmarkJson, rq.tagNameQuery);
+            InsertTags(rq.url, rq.json, rq.dbKey, rq.bookmarkJson, rq.tagNameQuery, false);
+            break;
+        case "ubm":
+            InsertTags(rq.url, rq.json, rq.dbKey, rq.bookmarkJson, rq.tagNameQuery, true, rq.id);
             break;
         case "sc-dbKey":
             chrome.cookies.set({
@@ -21,6 +29,9 @@ chrome.runtime.onMessage.addListener(function (rq, sender, sendResponse) {
                 name: "BookmarkKingDbUrl",
                 value: rq.data
             });
+            break;
+        case "dbm":
+            sendResponse(DeleteBookmark(rq.url, rq.dbKey));
             break;
         case "gc-dbKey":
             chrome.cookies.get({ "url": "https://www.vikyn.io", "name": "BookmarkKingDbKey" }, function (cookie) {
@@ -37,17 +48,49 @@ chrome.runtime.onMessage.addListener(function (rq, sender, sendResponse) {
                 sendResponse({ data: cookie.value });
             });
             break;
+        //GetBookmarkFolder
+        case "gbmf":
+            chrome.bookmarks.search(rq.data,
+                function (folders) {
+                    sendResponse({ data: folders });
+                });
+            break;
+        case "cbmf":
+            chrome.bookmarks.create({
+                'title': rq.data
+            });
+            break;
         case "cb":
             chrome.bookmarks.create({
-                'title': rq.url,
+                'parentId': rq.data.folderId,
+                'title': rq.data.name,
                 'url': rq.url
             });
             break;
+        default:
+            return true;
     }
     return true;  
 });
 
-function InsertTags(url, json, dbKey, bookmarkJson, tagNameQuery) {
+function DeleteBookmark(url, dbKey) {
+    $.ajax
+    ({
+        type: "DELETE",
+        url: url,
+        headers: {
+            "x-apikey": dbKey
+        },
+        success: function () {
+            return { response: "success" };
+        },
+        error: function () {
+            return { response: "error" };
+        }
+    });
+}
+
+function InsertTags(url, json, dbKey, bookmarkJson, tagNameQuery, update, id) {
     $.ajax
     ({
         type: "POST",
@@ -59,13 +102,13 @@ function InsertTags(url, json, dbKey, bookmarkJson, tagNameQuery) {
         },
         contentType: 'application/json',
         success: function() {
-            return GetTagsByName(url, bookmarkJson, dbKey, tagNameQuery);
+            return GetTagsByName(url, bookmarkJson, dbKey, tagNameQuery, update, id);
         },
         error: function () { return "error"; }
     });
 }
 
-function GetTagsByName(url, bookmarkJson, dbKey, tagNameQuery) {
+function GetTagsByName(url, bookmarkJson, dbKey, tagNameQuery, update, id) {
     $.ajax
     ({
         type: "GET",
@@ -77,7 +120,10 @@ function GetTagsByName(url, bookmarkJson, dbKey, tagNameQuery) {
         success: function(data) {
             var tagJson = CreateTagIdJson(data);
             var json = bookmarkJson.replace("{tags}", tagJson);
-            return InsertBookmark(url, json, dbKey);
+            if (update)
+                return UpdateBookmark(url, json, dbKey, id);
+            else
+                return InsertBookmark(url, json, dbKey);
         },
         error: function () { return false; }
     });
@@ -115,6 +161,25 @@ function InsertBookmark(url, json, dbKey) {
         error: function () { return false; }
     });
 }
+
+function UpdateBookmark(url, json, dbKey, id) {
+    $.ajax
+    ({
+        type: "PUT",
+        url: url + "bookmarks/" + id,
+        dataType: 'json',
+        data: json,
+        headers: {
+            "x-apikey": dbKey
+        },
+        contentType: 'application/json',
+        success: function () {
+            return true;
+        },
+        error: function () { return false; }
+    });
+}
+
 
 function GetTags(url, dbKey) {
     $.ajax
--- js/bookmarkKing.js
+++ js/bookmarkKing.js
@@ -1,6 +1,8 @@
 function BookmarkKing() {
     var dbUrlBase = "";
     var dbKey = "";
+    var currentTab = "";
+    var nameKeeper = "";
 
     function SetDbKey() {
         chrome.extension.sendMessage({
@@ -51,7 +53,6 @@ function BookmarkKing() {
             action: "rc"
         }, function (resp) {
             dbKey = resp.data;
-            console.log(dbKey);
         });
     }
 
@@ -59,14 +60,15 @@ function BookmarkKing() {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             var activeTab = tabs[0];
             var url = activeTab.url;
+            currentTab = url;
             AddBookmarkToDb(url, "");
             AddBookmarkToBrowser(url);
         });
     }
 
-    function SearchBookmarks() {
+    function SearchBookmarks(data) {
         var input9D4E63B7711C = document.getElementById('keyword_input_9D4E63B7711C');
-        var keywords = input9D4E63B7711C.value;
+        var keywords = data ? data : input9D4E63B7711C.value;
         if (!keywords)
             return;
         var tagQuery = TagsQueryBuilder(keywords);
@@ -105,19 +107,89 @@ function BookmarkKing() {
         var parsedJson = JSON.parse(json);
         var divDB32541AAB9B = document.getElementById('result_div_DB32541AAB9B');
         var html = "";
+        var ids = [];
         parsedJson.response.forEach(function(element) {
             var h = '<div style="padding-top:15px;">';
             h += '<a style="word-break: break-all" title="' + element.Url + '"  href="' + element.Url + '" target="_blank">' + element.Name + '</a>';
+            var id = CreateGuid() + element._id;
+            ids.push(id);
+            h += '&nbsp;<text style="cursor:pointer;" id="' + id + '">&#9998;</text>';
             h += "</div>";
             html += h;
         });
         divDB32541AAB9B.innerHTML = html;
+        ids.forEach(function (element) {
+            SetListenerForEditBookmarks(element);
+        });
+    }
+
+    function SetListenerForEditBookmarks(id) {
+        var htmlElement = document.getElementById(id);
+        htmlElement.addEventListener('click', function () {
+            var divDB325LNXA29B = document.getElementById("div_DB325LNXA29B");
+            var divDB325KA0929C = document.getElementById("div_DB325KA0929C");
+            divDB325LNXA29B.style.display = "block";
+            divDB325KA0929C.style.display = "none";
+            var id = this.id.split("_")[1];
+            chrome.extension.sendMessage({
+                action: "gb-id",
+                url: dbUrlBase + "bookmarks/" + id,
+                dbKey: dbKey
+            }, function (resp) {
+                    SetBookmarkData(JSON.stringify(resp));
+            });
+            });
+    }
+
+    var editBookmarkId = "";
+    var editBookmarkUrl = "";
+    function SetBookmarkData(json) {
+        var input9D4E63B7711C = document.getElementById('keyword_input_9D4E63B7711C');
+        var input9D4E69KA711C = document.getElementById('name_input_9D4E69KA711C');
+
+        var parsedJson = JSON.parse(json);
+        var tags = "";
+        parsedJson.response.Tags.forEach(function(element) {
+            if (tags === "")
+                tags += element.Name;
+            else
+                tags += ", " + element.Name;
+        });
+        input9D4E63B7711C.value = tags;
+        input9D4E69KA711C.value = parsedJson.response.Name;
+        editBookmarkId = parsedJson.response._id;
+        editBookmarkUrl = parsedJson.response.Url;
     }
 
     function AddBookmarkToBrowser(currentPageUrl) {
         chrome.extension.sendMessage({
+            action: "gbmf",
+            data: "Bookmark King"
+        }, function (resp) {
+                var folderId = "";
+                if (resp.data.length > 0) {
+                    folderId = resp.data[0].id;
+                    CreateBookmark(folderId, currentPageUrl);
+                }
+                else
+                    CreateBookmarkFolder(currentPageUrl);
+        });
+    }
+
+    function CreateBookmarkFolder() {
+        chrome.extension.sendMessage({
+                action: "cbmf",
+                data: "Bookmark King"
+        }, function (resp) {
+                AddBookmarkToBrowser(currentTab);
+        });
+    }
+
+    function CreateBookmark(folderId, currentPageUrl) {
+        chrome.extension.sendMessage({
             action: "cb",
-            url: currentPageUrl
+            url: currentPageUrl,
+            data: { folderId: folderId, name: nameKeeper }
         }, function (resp) {
         });
     }
@@ -132,6 +204,7 @@ function BookmarkKing() {
         if (!tagsJson.gtg)
             return;
         var name = !input9D4E69KA711C.value ? currentPageUrl : input9D4E69KA711C.value;
+        nameKeeper = name;
         var bookmarkJson = '{"Name": "' + name  + '", "Url" : "' + currentPageUrl + '", "Active": true, "Tags":[ {tags} ]}';
         chrome.extension.sendMessage({
             action: "kw",
@@ -224,6 +297,29 @@ function BookmarkKing() {
         });
     }
 
+    function UpdateBookmark() {
+        var input9D4E63B7711C = document.getElementById('keyword_input_9D4E63B7711C');
+        var input9D4E69KA711C = document.getElementById('name_input_9D4E69KA711C');
+        var keywords = input9D4E63B7711C.value;
+        if (!keywords)
+            return;
+        var tagsJson = CreateTagQuery(keywords);
+        if (!tagsJson.gtg)
+            return;
+        var name = !input9D4E69KA711C.value ? editBookmarkUrl : input9D4E69KA711C.value;
+        var bookmarkJson = '{"Name": "' + name + '", "Url" : "' + editBookmarkUrl + '", "Active": true, "Tags":[ {tags} ]}';
+        chrome.extension.sendMessage({
+            action: "ubm",
+            url: dbUrlBase,
+            id: editBookmarkId,
+            dbKey: dbKey,
+            json: tagsJson.tagsJson,
+            tagNameQuery: CreateTagQueryByName("or", tagsJson.cleanTagArray),
+            bookmarkJson: bookmarkJson
+        }, function (resp) { });
+        CancelEdit();
+    }
+
     function CheckForCookie() {
         var input9D4E63POX11C = document.getElementById('dbKey_input_9D4E63POX11C');
         var input9D4E69BNX11C = document.getElementById('dbUrl_input_9D4E69BNX11C');
@@ -239,6 +335,36 @@ function BookmarkKing() {
         });
     }
 
+    function CreateGuid() {
+        return 'xxxx-4xxx_'.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
+
+    function CancelEdit() {
+        var divDB325LNXA29B = document.getElementById("div_DB325LNXA29B");
+        var divDB325KA0929C = document.getElementById("div_DB325KA0929C");
+        var input9D4E63B7711C = document.getElementById('keyword_input_9D4E63B7711C');
+        var input9D4E69KA711C = document.getElementById('name_input_9D4E69KA711C');
+
+        divDB325LNXA29B.style.display = "none";
+        input9D4E63B7711C.value = "";
+        input9D4E69KA711C.value = "";
+        divDB325KA0929C.style.display = "block";
+    }
+
+    function DeleteBookmark() {
+        chrome.extension.sendMessage({
+            action: "dbm",
+            url: dbUrlBase + "bookmarks/" + editBookmarkId,
+            dbKey: dbKey
+        }, function (resp) {
+                SearchBookmarks();
+                CancelEdit();
+        });
+    }
+
     document.addEventListener('DOMContentLoaded', function () {
         var btnEA761612CD6D = document.getElementById('btn_search_EA761612CD6D');
         var btnA0CE92C0EC52 = document.getElementById('btn_add_A0CE92C0EC52');
@@ -247,13 +373,26 @@ function BookmarkKing() {
         var btnAPX992C0EC52 = document.getElementById('btn_close_APX992C0EC52');
         var btnKLX01612CD6D = document.getElementById('btn_save_KLX01612CD6D');
         var input9D4E69KA711C = document.getElementById('name_input_9D4E69KA711C');
+        var btnA0CE92CSAC52 = document.getElementById('btn_cancel_A0CE92CSAC52');
+        var btnASC0S12CFD6D = document.getElementById('btn_delete_ASC0S12CFD6D');
+        var btnPLSAW612CD6D = document.getElementById('btn_update_PLSAW612CD6D');
 
         // onClick's logic below:
+        btnASC0S12CFD6D.addEventListener('click', function () {
+            DeleteBookmark();
+        });
+        btnPLSAW612CD6D.addEventListener('click', function () {
+            UpdateBookmark();
+        });
+        btnA0CE92CSAC52.addEventListener('click', function () {
+            CancelEdit();
+        });
         btnA0CE92C0EC52.addEventListener('click', function () {
             AddBookmark();
         });
         btnEA761612CD6D.addEventListener('click', function () {
             SearchBookmarks();
+            CancelEdit();
         });
         icon9D4E63B77101.addEventListener('click', function () {
             CheckForCookie();
